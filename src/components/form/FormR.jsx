29be50006dc60e3935eb9b58cbd1eb0829fe65ad
Carlos Bugeau <cfbnostro@gmail.com>
2025-03-24 11:06:28 -0300

    Centre los botons de Login/Register y agregue el tituolo para Register

--- src/components/form/FormR.jsx
+++ src/components/form/FormR.jsx
@@ -75,40 +75,81 @@ const FormR = () => {
 
   return (
     <>
-      <Container className='d-flex justify-content-center my-5'>
+      <h3 className="d-flex justify-content-center">Formulario de Registro</h3>
+      <Container className="d-flex justify-content-center my-3">
         <Form>
           <Form.Group className="mb-3" controlId="formBasicEmail1">
             <Form.Label>Nombre de Usuario</Form.Label>
-            <Form.Control type="text" placeholder="Nombre Usuario" name='nombreUsuario' value={formRegister.nombreUsuario} onChange={handleChangeRegisterForm} isInvalid={errores.nombreUsuario} />
+            <Form.Control
+              type="text"
+              placeholder="Nombre Usuario"
+              name="nombreUsuario"
+              value={formRegister.nombreUsuario}
+              onChange={handleChangeRegisterForm}
+              isInvalid={errores.nombreUsuario}
+            />
           </Form.Group>
 
-          {
-              <Form.Group className="mb-3" controlId="formBasicEmail2">
-              <Form.Label>Email del Usuario</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" name='email' value={formRegister.email}  onChange={handleChangeRegisterForm} isInvalid={errores.email} />
-            </Form.Group>
-          }
+          <Form.Group className="mb-3" controlId="formBasicEmail2">
+            <Form.Label>Email del Usuario</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter email"
+              name="email"
+              value={formRegister.email}
+              onChange={handleChangeRegisterForm}
+              isInvalid={errores.email}
+            />
+          </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword1">
             <Form.Label>Contraseña</Form.Label>
-            <Form.Control type="password" placeholder="Password" name='contrasenia' value={formRegister.contrasenia} onChange={handleChangeRegisterForm} />
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              name="contrasenia"
+              value={formRegister.contrasenia}
+              onChange={handleChangeRegisterForm}
+            />
           </Form.Group>
-          {
-            <>
-              <Form.Group className="mb-3" controlId="formBasicPassword2">
-                <Form.Label>Repetir Contraseña</Form.Label>
-                <Form.Control type="password" placeholder="Password" name='repContrasenia' value={formRegister.repContrasenia} onChange={handleChangeRegisterForm} />
-              </Form.Group>
-              <Form.Group className="mb-3" controlId="formBasicCheckbox1">
-                <Form.Check type="checkbox" label="Check me out" name='checkForm' value={formRegister.checkForm} onChange={handleChangeRegisterForm}/>
-              </Form.Group>
-            </>
-          }
-          <Button variant="primary" type="submit" onClick={handleClickRegisterForm}>Grabar</Button>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword2">
+            <Form.Label>Repetir Contraseña</Form.Label>
+            <Form.Control
+              type="password"
+              placeholder="Password"
+              name="repContrasenia"
+              value={formRegister.repContrasenia}
+              onChange={handleChangeRegisterForm}
+            />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicCheckbox1">
+            <Form.Check
+              type="checkbox"
+              label="Tildar para continuar"
+              name="checkForm"
+              value={formRegister.checkForm}
+              onChange={handleChangeRegisterForm}
+            />
+          </Form.Group>
+
+          <Form.Group
+            className="d-flex justify-content-center"
+            controlId="formBasicButton1"
+          >
+            <Button
+              variant="primary"
+              type="submit"
+              onClick={handleClickRegisterForm}
+            >
+              Grabar
+            </Button>
+          </Form.Group>
         </Form>
       </Container>
     </>
-  )
+  );
 }
 
 export default FormR;
